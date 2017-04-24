12cdb1bcff221df2cebc2d4f4161eb7f0da752c0
Witoo Harianto <witoo@plimble.com>
2017-04-24 08:07:06 +0700

    update example

--- example/client/main.go
+++ example/client/main.go
@@ -1,72 +1,3 @@
-# jsonrpc
-Golang JSONRPC
-
-## Install
-
-#### Install Server
-
-```
-go get -u github.com/plimble/jsonrpc
-```
-
-#### Install client
-
-```
-go get -u github.com/plimble/jsonrpc/client
-```
-
-#### Install Both
-
-```
-go get -u github.com/plimble/jsonrpc/...
-```
-
-## Example Server
-
-```go
-package main
-
-import (
-	"context"
-
-	"github.com/plimble/jsonrpc"
-)
-
-type Adder struct{}
-
-type AddReq struct {
-	A, B int
-}
-
-type AddRes struct {
-	Val int
-}
-
-func (a *Adder) Add(ctx context.Context, req *AddReq) (*AddRes, error) {
-	val := req.A + req.B
-	return &AddRes{
-		Val: val,
-	}, nil
-}
-
-func (a *Adder) Multiply(ctx context.Context, req *AddReq) (*AddRes, error) {
-	val := req.A * req.B
-	return &AddRes{
-		Val: val,
-	}, nil
-}
-
-func main() {
-	j := jsonrpc.New()
-	j.RegisterService(new(Adder), "adder")
-
-	j.Listen(":3000")
-}
-```
-
-## Example Client
-
-```go
 package main
 
 import (
@@ -107,5 +38,3 @@ func main() {
 		fmt.Println("###", result)
 	}
 }
-```
-
